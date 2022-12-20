b3010fa863f55a5e6b92b47aba437a950bde41ee
Mehmet Ali Demir <64283995+mehmetalidemir@users.noreply.github.com>
2022-12-20 17:13:42 +0300

    created project

---
+++ week-3-homework-1/week-3-homework-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week-3-homework-1
+//
+//  Created by Mehmet Ali Demir on 20.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
